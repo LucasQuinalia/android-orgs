c0f16aa4034e15ebddd8ecd2809bd9953e252f48
Lucas Quinália <lrgquinalia@gmail.com>
2025-02-06 12:42:35 -0300

    first commit

---
+++ app/src/main/java/com/example/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.orgs.ui.recyclerview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.databinding.ProdutoItemBinding
+import com.example.orgs.model.Produto
+
+class ListaProdutosAdapter(
+    private val context: Context,
+    produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    private val produtos = produtos.toMutableList()
+
+    class ViewHolder(binding: ProdutoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        val nome = binding.produtoItemNome
+        val descricao = binding.produtoItemDescricao
+        val valor = binding.produtoItemValor
+
+        fun vincula(produto: Produto) {
+            nome.text = produto.nome
+            descricao.text = produto.descricao
+            valor.text = produto.valor.toPlainString()
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ProdutoItemBinding
+            .inflate(
+                LayoutInflater.from(context),
+                parent,
+                false
+            )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
+    }
+
+    override fun getItemCount(): Int = produtos.size
+
+    fun atualiza(produtos: List<Produto>) {
+        this.produtos.clear()
+        this.produtos.addAll(produtos)
+        notifyDataSetChanged()
+    }
+
+}
